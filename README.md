1ff3b969385ae74e3e7efe1060b56ab120dae7bd
juanse217 <juansebas08004@gmail.com>
2025-08-31 22:17:25 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+##
+## CLASS 7, USE OF JUNIT FOR TESTS
+
+In this class we check the core functionality of Junit by implementing a simple calculator class and its tests. 
---
+++ src/main/java/com/sebastian/Main.java
@@ -0,0 +1,7 @@
+package com.sebastian;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("PROYECTO CLASE 7 PRUEBAS CON JUNIT");
+    }
+}
