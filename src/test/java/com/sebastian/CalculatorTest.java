48554a245f423f784983b864c69dfc1328218150
juanse217 <juansebas08004@gmail.com>
2025-09-02 18:06:53 -0500

    using other annotations

--- src/test/java/com/sebastian/CalculatorTest.java
+++ src/test/java/com/sebastian/CalculatorTest.java
@@ -2,39 +2,53 @@ package com.sebastian;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+    private Calculator calc; 
+
+    @BeforeEach
+    void initializer(){
+        calc = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown(){
+        calc = null;
+    }
     @Test
     void sumTest() {
-        Calculator calc = new Calculator();
         assertEquals(5, calc.sum(2, 3));
     }
 
     @Test
     void substractionTest() {
-        Calculator calc = new Calculator();
         assertEquals(5, calc.substraction(10, 5));
     }
 
     @Test
     void multiplicationTest() {
-        Calculator calc = new Calculator();
         assertEquals(6, calc.multiplication(2, 3));
     }
 
     @Test
     void divisionTest() {
-        Calculator calc = new Calculator();
         assertEquals(3, calc.division(6, 2));
     }
 
     @Test
+    @DisplayName("ExceptionChecking")
     void divisionExceptionTest() {
         Calculator calc = new Calculator();
-        assertThrows(ArithmeticException.class, () -> {
+        ArithmeticException excep = assertThrows(ArithmeticException.class, () -> {
             calc.division(4, 0);
         });
+
+        assertTrue(excep.getMessage().equalsIgnoreCase("the divisor can't be 0"), "the error message should let the user know they can't divide by 0");
     }
 }
