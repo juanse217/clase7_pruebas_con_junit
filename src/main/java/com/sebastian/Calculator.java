a9db23589527afb07db3df621aecd64b8e3bf109
juanse217 <juansebas08004@gmail.com>
2025-09-02 17:37:11 -0500

    Tests passed

---
+++ src/main/java/com/sebastian/Calculator.java
@@ -0,0 +1,20 @@
+package com.sebastian;
+
+public class Calculator {
+    
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    public int substraction(int a, int b){
+        return a - b; 
+    }
+
+    public int multiplication(int a, int b){
+        return a * b;
+    }
+
+    public int division(int a, int b){
+        return  a / b;
+    }
+}
---
+++ src/test/java/com/sebastian/CalculatorTest.java
@@ -0,0 +1,31 @@
+package com.sebastian;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    void sumTest(){
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.sum(2, 3));
+    }
+
+    @Test
+    void substractionTest(){
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.substraction(10, 5));
+    }
+
+    @Test
+    void multiplicationTest(){
+        Calculator calc = new Calculator();
+        assertEquals(6, calc.multiplication(2, 3));
+    }
+
+    @Test
+    void divisionTest(){
+        Calculator calc = new Calculator();
+        assertEquals(3, calc.division(6, 2));
+    }
+}
